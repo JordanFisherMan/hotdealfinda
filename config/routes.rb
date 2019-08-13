024e4d977293e3c7fd6074225a0ce5b9c3a2c89a
Jordan Fisher <jordanfisher@windowslive.com>
2019-08-13 16:14:52 +1200

    began creating about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'application#index'
   get 'results', to: 'results#results'
+  get 'about', to: 'application#about'
 end
