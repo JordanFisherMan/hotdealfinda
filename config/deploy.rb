5b66db6eb2e41700a5e177243cbacdae6a582977
Jordan Fisher <jordanfisher@windowslive.com>
2020-08-17 00:03:52 +1200

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,3 +76,16 @@ set :whenever_variables, -> {
 }
 
 after 'deploy:updated', 'webpacker:precompile'
+
+before "deploy:assets:precompile", "deploy:yarn_install"
+
+namespace :deploy do
+  desc 'Run rake yarn:install'
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install")
+      end
+    end
+  end
+end
