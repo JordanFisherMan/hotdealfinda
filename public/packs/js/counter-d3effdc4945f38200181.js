74b458b22067e7afc6dd7eb60e74ee4d8d563864
Jordan Fisher <jordanfisher@windowslive.com>
2020-12-02 23:37:03 +1300

    packs

---
+++ public/packs/js/counter-d3effdc4945f38200181.js
@@ -0,0 +1,144 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/packs/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./app/javascript/packs/counter.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./app/javascript/counter/index.js":
+/*!*****************************************!*\
+  !*** ./app/javascript/counter/index.js ***!
+  \*****************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// A simple counter example
+// The setup will be more complicated in modern apps built using React
+var incrementNode = document.getElementById('increment');
+var decrementNode = document.getElementById('decrement');
+var inputNode = document.getElementById('counter');
+var counter = {
+  initialize: function initialize() {
+    incrementNode.addEventListener('click', function (event) {
+      event.preventDefault();
+      var currentValue = inputNode.value;
+      inputNode.value = parseInt(currentValue, 0) + 1;
+    });
+    decrementNode.addEventListener('click', function (event) {
+      event.preventDefault();
+      var currentValue = inputNode.value;
+
+      if (currentValue > 0) {
+        inputNode.value = parseInt(currentValue, 0) - 1;
+      }
+    });
+  }
+};
+/* harmony default export */ __webpack_exports__["default"] = (counter);
+
+/***/ }),
+
+/***/ "./app/javascript/packs/counter.js":
+/*!*****************************************!*\
+  !*** ./app/javascript/packs/counter.js ***!
+  \*****************************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _counter_index__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../counter/index */ "./app/javascript/counter/index.js");
+// Require or import the counter module
+ // Initialize the counter code when DOM is ready
+
+document.addEventListener('DOMContentLoaded', function () {
+  _counter_index__WEBPACK_IMPORTED_MODULE_0__["default"].initialize();
+});
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=counter-d3effdc4945f38200181.js.map
